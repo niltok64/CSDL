a7ffae61ea13f2788f32583872711fdd1d07c840
imsad2 <72703236+dastikman@users.noreply.github.com>
2021-12-30 19:50:52 +0000

    v1.2(add deluxe pack)

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ import os
 doukutsu_url = "https://studiopixel.jp/binaries/dou_1006.zip"
 cavestory_enpatch = "https://aeongenesis.net/download/cavestory"
 cavestory_prepatch_en = "https://www.cavestory.org/downloads/cavestoryen.zip"
+deluxepack_updated = "https://www.cavestory.org/downloads/Cave_Story_Deluxe.exe"
+deluxepack_old = "https://www.cavestory.org/downloads/Cave_Story_Deluxe_old.exe"
 
 tempfolder = mkdtemp()
 
@@ -33,10 +35,25 @@ def download():
         info("Finished", "Cave Story has been extracted in the temp folder")
         os.remove("cavestory.zip")
 
-app = App(title="Cave Story Downloader")
+def download_dx():
+    if deluxeversion.value == "DP":
+        dlxupd = get(deluxepack_updated, allow_redirects=True)
+        open('cavestorydx.exe', 'wb').write(dlxupd.content)
+        info("Finished", "The Cave Story Deluxe EXE has finished downloading")
+    else:
+        dlxold = get(deluxepack_old, allow_redirects=True)
+        open('cavestorydx_old.exe', 'wb').write(dlxold.content)
+        info("Finished", "The old Cave Story Deluxe EXE has finished downloading")
+
+app = App(title="Cave Story Downloader", height=550, width=500)
 
 cavestorypic = Picture(app, image="logo.png")
+
+normal_text = Text(app, text="Standard version")
 version = ButtonGroup(app, options=[["Doukutsu Monogatari", "DC"], ["Cave Story English Patch", "EP"], ["Cave Story English Pre-Patched", "PP"]], selected="DC")
 download_button = PushButton(app, command=download, text="Download and Extract")
+deluxepack_text = Text(app, text="Deluxe pack")
+deluxeversion = ButtonGroup(app, options=[["Cave Story Deluxe Pack", "DP"], ["Old Cave Story Deluxe Pack", "ODP"]], selected="DP")
+download_button_dx = PushButton(app, command=download_dx, text="Download Deluxe Pack")
 
 app.display()
