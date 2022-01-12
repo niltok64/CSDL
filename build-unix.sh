afa6247cd484a4270968cb06d57e73379d3c5bba
Imsad2 <72703236+imsad2@users.noreply.github.com>
2022-01-12 15:02:16 +0000

    Create build-unix.sh

---
+++ build-unix.sh
@@ -0,0 +1,8 @@
+echo BUILDING DEM CODE 4 UNIX LOL
+echo REMEMBER DAT U NEED PYTHON ON DEM PATH LMAO
+pip install pyinstaller
+cd ..
+mkdir compile
+pyinstaller main.py --onefile -w --name CaveStoryDL --distpath compile
+echo DONE BUILDING LMAO
+echo REMEMBER TO PUT DA LOGO IN COMPILE FOLDER AND FROLIC YOOO
