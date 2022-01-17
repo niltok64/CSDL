0ddb97849f66929fd09ba4a27075c3b2220c9893
Imsad2 <72703236+imsad2@users.noreply.github.com>
2022-01-17 08:59:23 +0000

    Update win-release.bat

--- builders/win-release.bat
+++ builders/win-release.bat
@@ -3,7 +3,7 @@ echo BUILDING FOR RELEASE
 cd ..
 mkdir release
 pip install pyinstaller
-pyinstaller main.py --distpath release --onefile -w -n CSD --windowed --add-data "logo.png"
+pyinstaller main.py --distpath release --onefile -w -n CSD --windowed --add-data "logo.png" --add-data "tempfolder/"
 cd release
 powershell Compress-Archive -LiteralPath ".\CSD" -DestinationPath ".\CaveStoryDownloader.zip"
 echo DONE BUILDIN, RELEASE IS IN \release LMAO
