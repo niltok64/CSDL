ed85dbd6fee1ffaf9b328c97c2b347c2442c68d6
Imsad2 <72703236+imsad2@users.noreply.github.com>
2022-01-17 08:59:48 +0000

    Update build-unix.sh

--- builders/build-unix.sh
+++ builders/build-unix.sh
@@ -4,5 +4,5 @@ echo REMEMBER DAT U NEED PYTHON ON DEM PATH LMAO
 pip install pyinstaller
 cd ..
 mkdir compile
-pyinstaller main.py --onefile -w --name CaveStoryDL --distpath compile --add-data "logo.png"
+pyinstaller main.py --onefile -w --name CaveStoryDL --distpath compile --add-data "logo.png" --add-data "tempfolder/"
 echo DONE BUILDING LMAO
