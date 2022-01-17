dd9de76db5efbeba762d7a7712dfa9a4500b71f1
Imsad2 <72703236+imsad2@users.noreply.github.com>
2022-01-17 08:59:05 +0000

    Update build-win.bat

--- builders/build-win.bat
+++ builders/build-win.bat
@@ -4,6 +4,6 @@ ECHO REMEMBER DAT U NEED PYTHON ON DEM PATH LMAO
 pip install pyinstaller
 cd ..
 mkdir compile
-pyinstaller main.py --onefile -w --name CaveStoryDL --distpath compile --add-data "logo.png"
+pyinstaller main.py --onefile -w --name CaveStoryDL --distpath compile --add-data "logo.png" --add-data "tempfolder/"
 ECHO DONE BUILDING LMAO
 PAUSE
