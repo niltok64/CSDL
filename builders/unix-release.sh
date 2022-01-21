c262ed57c72d52b519b28601ab7bd7edeb753d50
Imsad2 <72703236+imsad2@users.noreply.github.com>
2022-01-21 12:32:13 +0000

    Update unix-release.sh

--- builders/unix-release.sh
+++ builders/unix-release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-echo BUILDING
+echo "BUILDING"
 cd ..
 mkdir release-unix
 pyinstaller main.py --onefile -w -n CSD --distpath release-unix --add-data icon.png --add-data "tempfolder/"
 cd release-unix
 zip -r9 "CaveStoryDownloader-Linux" "CSD/"
-echo DONE BUILDIN, RELEASE IN /release LMAO
+echo "DONE BUILDIN, RELEASE IN /release LMAO"
