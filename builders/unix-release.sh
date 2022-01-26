f260485f32424089e564073d9a6cfe5c663973a2
imsad2 <72703236+dastikman@users.noreply.github.com>
2022-01-26 18:18:27 +0000

    commit 1 for unix version

--- builders/unix-release.sh
+++ builders/unix-release.sh
@@ -3,7 +3,7 @@ echo "BUILDING"
 cd ..
 cd src
 mkdir release-unix
-pyinstaller main-unix.py --onefile -w -n CSD --distpath release-unix --add-data icon.png --add-data "tempfolder/"
+pyinstaller main-unix.py --onefile -w -n CSD --distpath release-unix --add-data "logo-unix.png" --add-data "tempfolder/"
 cd release-unix
 zip -r9 "CaveStoryDownloader-Unix" "CSD/"
 echo "DONE BUILDIN, RELEASE IN /release LMAO"
--- builders/win-release.bat
+++ builders/win-release.bat
@@ -4,7 +4,7 @@ cd ..
 cd src
 mkdir release
 pip install pyinstaller
-pyinstaller main-windows.py --distpath release --onefile -w -n CSD --windowed --add-data "logo.png" --add-data "tempfolder/"
+pyinstaller main-windows.py --distpath release --onefile -w -n CSD --windowed --add-data "logo-win.png" --add-data "tempfolder/"
 cd release
 powershell Compress-Archive -LiteralPath ".\CSD" -DestinationPath ".\CaveStoryDownloader.zip"
 echo DONE BUILDIN, RELEASE IS IN \release LMAO
